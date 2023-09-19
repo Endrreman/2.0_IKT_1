203aff68a0fa2bfecc0acdd710ee762bee90a1e9
Endrreman <nagy.endre.bulcsu@gmail.com>
2023-09-19 10:16:29 +0200

    1.3
    teszt(tanlán jó lesz a menu col)

---
+++ General/anims.js
@@ -0,0 +1,15 @@
+//!         __Menu
+    //*         _menu.html
+        //         col load
+
+document.addEventListener('animationstart', function (e) {
+    if (e.animationName === 'fade-in') {
+        e.target.classList.add('did-fade-in');
+    }
+});
+  
+document.addEventListener('animationend', function (e) {
+    if (e.animationName === 'fade-out') {
+        e.target.classList.remove('did-fade-in');
+     }
+});
