835fa64fd8d585b1e454bb0d1539f1b91424fcc6
Endrreman <nagy.endre.bulcsu@gmail.com>
2023-10-10 17:24:55 +0200

    10.10_1.0

---
+++ General/anims.js
@@ -0,0 +1,51 @@
+//!         General
+Splitting();
+
+//!         Nav
+
+//*     neu_120
+var bio = document.getElementById("nav_bio");
+var iskola = document.getElementById("nav_sajat");
+var tanul = document.getElementById("nav_tanul");
+var iskolank = document.getElementById("nav_iskolank");
+
+ScrollOut({
+    targets:"#nav-1",
+    onShown() {
+        bio.classList.add("active");
+    },
+    onHidden() {
+        bio.classList.remove("active");
+    },
+    threshold: 0.6,
+});
+ScrollOut({
+    targets:"#nav-2",
+    onShown() {
+        iskola.classList.add("active");
+    },
+    onHidden() {
+        iskola.classList.remove("active");
+    },
+    threshold: 0.6,
+});
+ScrollOut({
+    targets:"#nav-3",
+    onShown() {
+        tanul.classList.add("active");
+    },
+    onHidden() {
+        tanul.classList.remove("active");
+    },
+    threshold: 0.3,
+});
+ScrollOut({
+    targets:"#nav-4",
+    onShown() {
+        iskolank.classList.add("active");
+    },
+    onHidden() {
+        iskolank.classList.remove("active");
+    },
+    threshold: 0.6,
+});
